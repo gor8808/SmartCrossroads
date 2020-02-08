a29f1fb31e6d1dd0c92a1b56215aea4f1d28863c
Gor Grigoryan <gor8808@users.noreply.github.com>
2020-02-08 11:23:02 +0400

    Creating files

---
+++ SmartCrossroads/SmartCrossroads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartCrossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
